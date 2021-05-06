d840370e0fd459e8167b3abaff6255985036e9b3
64Romaine64 <g.romain64@gmail.com>
2021-05-06 12:30:21 +0200

    optimisation

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Repository\PhotoRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,11 +13,14 @@ class AboutController extends AbstractController
     /**
      * @Route("/a-propos", name="about")
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, PhotoRepository $photoRepository): Response
     {
+
+        $photo = $photoRepository->findBy(['nom' => 'photoAbout']);
 
         return $this->render('about/index.html.twig', [
             'photographe' => $userRepository->getPhotographe(),
+            'photo' => $photo,
         ]);
         
     }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,23 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use App\Repository\UserRepository;
 use App\Service\ContactService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use App\Repository\PhotoRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request, ContactService $contactService, UserRepository $userRepository): Response
+    public function index(Request $request, ContactService $contactService, UserRepository $userRepository, PhotoRepository $photoRepository): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
+        $photo = $photoRepository->findBy(['nom' => 'photoContact']);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $contact = $form->getData();
@@ -32,6 +34,7 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'photographe' => $userRepository->getPhotographe(),
             'form' => $form->createView(),
+            'photo' => $photo,
         ]);
     }
 }
